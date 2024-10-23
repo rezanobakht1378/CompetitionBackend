ba84451d466330d9651e2b810ff21bcbe1f2b799
EZano <unknown1pro@gmail.com>
2024-10-23 14:24:20 +0330

    fix competiotion and user table

--- database/migrations/0001_01_01_000000_create_competitions_table.php
+++ database/migrations/0001_01_01_000000_create_competitions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('competitions', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
